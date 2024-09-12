2abf9329d90e1ac631b4f11ed67d949cf5deef84
Vsevolod <sevach92@yandex.ru>
2024-09-12 09:54:56 +0400

    Add files via upload

---
+++ lesson 0.py
@@ -0,0 +1,17 @@
+# 1st program
+print (9**0.5*5)
+# 2en program
+print (9.99>9.98,  1000!=1000.1)
+# 3rd program
+print (2*2+2)
+print (2*(2+2))
+print ((2*2+2) == (2*(2+2)))
+# 4th program
+print (float('123.456'))
+print (10*float('123.456'))
+print (int(10*float('123.456')))
+print (int(10*float('123.456'))-1194)
+print (int((10*float('123.456'))-1194)//10)
+# или более простой способ извлечения цифры 4
+name = '123.456'
+print (name[4])
