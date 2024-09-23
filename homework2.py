cdc80dd4ebbbce98480b9cf697d10d3490af500a
Vsevolod <sevach92@yandex.ru>
2024-09-23 20:05:24 +0400

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,6 @@
+homework_count = 'всего задач: 12,'
+hour_count = 'затрачено часов: 1.5,'
+name_course = 'Курс: Python,'
+possible_time = 'среднее время выполнения 0.125 часа.'
+print (name_course, homework_count, hour_count, possible_time)
+
